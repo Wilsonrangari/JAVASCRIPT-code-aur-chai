be37911d0506470c3b464f1cc38656fcd4a8e172
Wilson <rangariwilson@gmail.com>
2023-09-28 12:01:48 +0200

    object updated

--- 01_basics/04_objects.js
+++ 01_basics/04_objects.js
@@ -11,15 +11,53 @@ tinderUser.age = 28
 // console.log(tinderUser);
 
 
-const regularuser = {
+// const regularuser = {
 
-  email:"some@gmail.com",
-  fullname:{
-    userfullname:{
-      firstname: "Wilson",
-      lastname:"Rangari"
-    }
+//   email:"some@gmail.com",
+//   fullname:{
+//     userfullname:{
+//       firstname: "Wilson",
+//       lastname:"Rangari"
+//     }
+//   }
+// }
+
+// console.log(regularuser.fullname.userfullname.firstname);
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+
+//const obj3 = {obj1, obj2} we dont want this output
+//console.log(obj3);
+//o/p should be {1:"a", 2:"b", 3:"a", 4:"b"} so use below method
+//const obj3 =  Object.assign({}, obj1, obj2)  //--> here {} is like a target and obj1 and obj2 is source
+//console.log(obj3);
+//another way to split
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+//ARRAY OF OBJECTS
+
+const users = [
+  {
+    id : 1,
+    email : "h@gmil.com"
+  }, 
+  {
+    id: 2,
+    email : "w@y.com"
   }
-}
+]
 
-console.log(regularuser.fullname.userfullname.firstname);
+console.log(users[1].email); 
+console.log(users[1]);
+
+
+
+
+
+
+
+
+
